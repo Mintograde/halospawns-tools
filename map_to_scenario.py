a21b4ad7b7ffa649a1642f808a8619ab30d07dbd
Mintograde <mintograde@gmail.com>
2020-01-13 19:26:37 -0500

    Add equipment and teleporters to exported json

--- map_to_scenario.py
+++ map_to_scenario.py
@@ -16,6 +16,7 @@
 import json
 import os
 import shutil
+from collections import defaultdict
 
 from refinery import core
 
@@ -82,7 +83,7 @@ def map_to_scenario(filename, base_directory, delete_tags_directory=True):
     spawns_array = meta.player_starting_locations.player_starting_locations_array
     spawns_list = []
     for i, spawn in enumerate(spawns_array):
-        spawn_dict = dict(
+        spawn_item = dict(
             x=spawn.position.x,
             y=spawn.position.y,
             z=spawn.position.z,
@@ -99,14 +100,52 @@ def map_to_scenario(filename, base_directory, delete_tags_directory=True):
             type_3_name=spawn.type_3.enum_name,
             spawn_index=i,
         )
-        spawns_list.append(spawn_dict)
+        spawns_list.append(spawn_item)
+
+    equip_array = meta.netgame_equipments.netgame_equipments_array
+    equip_list = []
+    for i, equip in enumerate(equip_array):
+        equip_item = dict(
+            x=equip.position.x,
+            y=equip.position.y,
+            z=equip.position.z,
+            type_0=equip.type_0.data,
+            type_1=equip.type_1.data,
+            type_2=equip.type_2.data,
+            type_3=equip.type_3.data,
+            facing=equip.facing,
+            team_index=equip.team_index,
+            spawn_time=equip.spawn_time,
+            filepath=equip.item_collection.filepath,
+            levitate=equip.flags.get('levitate'),
+            equip_index=i,
+        )
+        equip_list.append(equip_item)
+
+    flags_array = meta.netgame_flags.netgame_flags_array
+    flags_list = []
+    teleporters = defaultdict(dict)
+    for i, flag in enumerate(flags_array):
+        flag_item = dict(
+            x=flag.position.x,
+            y=flag.position.y,
+            z=flag.position.z,
+            facing=flag.facing,
+            team_index=flag.team_index,
+            flag_type=flag.type.data,
+            flag_type_name=flag.type.enum_name,
+            flags_index=i,
+        )
+        if flag.type.enum_name in ('teleport_from', 'teleport_to'):
+            teleporters[flag.team_index][flag.type.enum_name] = flag_item
+        flags_list.append(flag_item)
 
     meta_json = json.dumps(
         dict(
             spawns=spawns_list,
             scenery=[],
-            teleporters=[],
-            equipment=[],
+            teleporters=teleporters,
+            equipment=equip_list,
         )
     )
     meta_json_path = os.path.join(os.path.join(os.path.dirname(scenario_path), f'{parent_directory}.json'))
